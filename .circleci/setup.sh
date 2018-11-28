8e43aa7429a1babf52b015ebfe614340cb1447dc
varunnotkar59 <varunnotkar59@gmail.com>
2018-11-28 13:13:57 +0000

    Automated deployment to GitHub Pages

--- .circleci/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.email "${GH_EMAIL}" > /dev/null 2>&1
-git config --global user.name "${GH_NAME}" > /dev/null 2>&1
-
-cat >> ~/.ssh/config << EOF
-VerifyHostKeyDNS yes
-StrictHostKeyChecking no
-EOF

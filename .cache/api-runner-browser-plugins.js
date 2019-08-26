8836c3d0128f18269071215fadcd36351545628c
Charan <charan.r@pacewisdom.com>
2019-08-26 12:46:02 +0530

    Added about-us content

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Projects/Zysk/magikbox.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Projects/Zysk/magikbox.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- styles-a746371f4e9f348334d8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{187:function(n,o,w){},407:function(n,o,w){},420:function(n,o,w){}}]);
-//# sourceMappingURL=styles-a746371f4e9f348334d8.js.map

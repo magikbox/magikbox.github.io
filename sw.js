bd081b8141760fd80e0d181fa3cccc8551f15f5b
varunumesh <varun@zysk.in>
2020-06-01 16:15:23 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b5b1b147de1f5ef258cd.js"
+    "url": "webpack-runtime-75a75070f7252a7e684d.js"
   },
   {
     "url": "framework-3c89b73af5a16e4505f4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "365944daa063ec8eded29e85bbd460c9"
+    "revision": "a222ec6d360970d442314fe08312c1c7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4b94bce5c721474edc44.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7457777e5a76bd7f05d9e370de08011c"
+    "revision": "45fe0071d379135c00a2226352abc1d4"
   },
   {
     "url": "manifest.webmanifest",
